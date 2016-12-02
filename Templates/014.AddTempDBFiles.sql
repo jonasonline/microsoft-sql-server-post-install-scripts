f54fbd5d733843521d220e5661bee4401ccad10a
Jonas Carlsson <jonas@jonasonline.se>
2016-12-02 10:27:36 +0100

    Fixing bugs

--- Templates/014.AddTempDBFiles.sql
+++ Templates/014.AddTempDBFiles.sql
@@ -1,21 +1,20 @@
 USE [master]
-
 GO
-
 ALTER DATABASE [tempdb] MODIFY FILE ( NAME = N'tempdev', SIZE = 1024000KB , FILEGROWTH = 65536KB )
-
 GO
-
 ALTER DATABASE [tempdb] MODIFY FILE ( NAME = N'templog', SIZE = 512000KB , FILEGROWTH = 65536KB )
-
 GO
 
 DECLARE @CPU_Count int = (SELECT cpu_count FROM sys.dm_os_sys_info), 
-		@i int = 1, 
+		@i int = 1,
 		@LogicalName nvarchar(500),
-		@LogicalName_rev nvarchar(500), 
-		@PhysicalName nvarchar(500), 
-		@SQLCommand nvarchar(2000)
+		@BaseLogicalName nvarchar(500), 
+		@BaseLogicalName_rev nvarchar(500), 
+		@PhysicalName nvarchar(500),
+		@BasePhysicalName nvarchar(500),
+		@BasePhysicalName_rev nvarchar(500), 
+		@SQLCommand nvarchar(2000),
+		@MaxNumberOfFiles int = 8
 
 DECLARE @NumberOfExistingTempdbFiles INT
 
@@ -24,25 +23,29 @@ JOIN sys.databases b on a.database_id = b.database_id
 WHERE b.name = 'tempdb' AND a.type = 0
 
 SELECT @PhysicalName = REPLACE(physical_name, '.mdf', '.ndf') FROM sys.master_files WHERE name = 'tempdev'
-SET @PhysicalName = REPLACE(@PhysicalName, 'tempdb.ndf', 'tempdb_mssql_1.ndf')
-SET @LogicalName = N'temp1'
+SET @BasePhysicalName = N'tempdb_mssql_' + CAST(@NumberOfExistingTempdbFiles AS nvarchar(200)) + '.ndf'
+SET @PhysicalName = REPLACE(@PhysicalName, 'tempdb.ndf', @BasePhysicalName)
+SET @BaseLogicalName = N'temp' + CAST(@NumberOfExistingTempdbFiles AS nvarchar(200))
+SET @LogicalName = @BaseLogicalName
 
 DECLARE @NumberOfFilesToCreate INT = @CPU_Count
-
-IF @NumberOfFilesToCreate > 8
+IF @NumberOfFilesToCreate > @MaxNumberOfFiles
 BEGIN
-	SET @NumberOfFilesToCreate = 8
+	SET @NumberOfFilesToCreate = @MaxNumberOfFiles
 END
-
-IF @NumberOfExistingTempdbFiles < @NumberOfFilesToCreate
+SET @NumberOfFilesToCreate = @NumberOfFilesToCreate - @NumberOfExistingTempdbFiles
 BEGIN
-	WHILE @i < @NumberOfFilesToCreate OR @i <= 8
+	WHILE @i <= @NumberOfFilesToCreate
 	BEGIN
-		SET @LogicalName_rev = REPLACE(@LogicalName, @i, @i +1)
-		SET @PhysicalName = REPLACE(@PhysicalName, @LogicalName, @LogicalName_rev)
-		SET @SQLCommand = 'ALTER DATABASE [tempdb] ADD FILE ( NAME = N''' + @LogicalName_rev + ''', FILENAME = N''' + @PhysicalName + ''', SIZE = 1024000KB , FILEGROWTH = 65536KB )'
+		SET @BaseLogicalName_rev = REPLACE(@BaseLogicalName, @NumberOfExistingTempdbFiles + @i -1, @i + @NumberOfExistingTempdbFiles)
+		SET @BasePhysicalName_rev = REPLACE(@BasePhysicalName, @NumberOfExistingTempdbFiles + @i -1, @i + @NumberOfExistingTempdbFiles)
+		SET @LogicalName = REPLACE(@LogicalName, @LogicalName, @BaseLogicalName_rev)
+		SET @PhysicalName = REPLACE(@PhysicalName, @BasePhysicalName, @BasePhysicalName_rev)
+		PRINT @LogicalName + ' ' + @PhysicalName
+		SET @SQLCommand = 'ALTER DATABASE [tempdb] ADD FILE ( NAME = N''' + @LogicalName + ''', FILENAME = N''' + @PhysicalName + ''', SIZE = 1024000KB , FILEGROWTH = 65536KB )'
 		EXEC (@SQLCommand)
 		SET @i = @i +1
-		SET @LogicalName = @LogicalName_rev 
+		SET @BaseLogicalName = @BaseLogicalName_rev 
+		SET @BasePhysicalName = @BasePhysicalName_rev
 	END
 END 
